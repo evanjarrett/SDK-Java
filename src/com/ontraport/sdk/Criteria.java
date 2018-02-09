fb676094adc8ccde2e7a9c7215a37b6f03a896ab
Evan Jarrett <ejfirestar00@gmail.com>
2018-02-09 14:46:20 -0800

    gradle-ify

--- src/com/ontraport/sdk/Criteria.java
+++ src/com/ontraport/sdk/Criteria.java
@@ -1,4 +1,19 @@
 package com.ontraport.sdk;
 
 public class Criteria {
+
+    protected String[] _condition;
+
+    public Criteria(String field, String operator, String value) {
+        _condition = buildCondition(field, operator, value);
+    }
+
+    public String fromArray() {
+        JsonObject obj = Json.createObjectBuilder().build();
+        return "";
+    }
+
+    private String[] buildCondition(String field, String operator, String value) {
+
+    }
 }
