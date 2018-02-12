99f623037fe9d0c3c25e2c931348db31f2dcf687
Evan Jarrett <ejfirestar00@gmail.com>
2018-02-11 17:04:11 -0800

    need a way to get this data

--- src/main/java/com/ontraport/sdk/http/ListResponse.java
+++ src/main/java/com/ontraport/sdk/http/ListResponse.java
@@ -4,4 +4,8 @@ import java.util.Map;
 
 public class ListResponse extends AbstractResponse {
     private Map<String,String>[] data;
+
+    public Map<String,String>[] getData() {
+        return data;
+    }
 }
