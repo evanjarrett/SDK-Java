83edb89d901420be9d633beb32546408bf5a5731
Evan Jarrett <ejfirestar00@gmail.com>
2018-02-14 22:36:07 -0800

    fixup Meta

--- src/main/java/com/ontraport/sdk/http/Meta.java
+++ src/main/java/com/ontraport/sdk/http/Meta.java
@@ -7,7 +7,15 @@ public class Meta extends AbstractResponse {
 
     public class Data implements AbstractResponse.Data {
         String name;
-        Map<String,Field> fields;
+        Map<String, Field> fields;
+
+        public String getName() {
+            return name;
+        }
+
+        public Map<String, Field> getFields() {
+            return fields;
+        }
     }
 
     public class Field {
@@ -15,6 +23,30 @@ public class Meta extends AbstractResponse {
         String type;
         String parent_object;
         Map<String, String> options;
+
+        public String getAlias() {
+            return alias;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public boolean hasParent() {
+            return type.equals("parent") && parent_object != null;
+        }
+
+        public String getParent() {
+            return parent_object;
+        }
+
+        public boolean hasOptions() {
+            return (type.equals("drop") || type.equals("list")) && options != null;
+        }
+
+        public Map<String, String> getOptions() {
+            return options;
+        }
     }
 
     public Map<String, Data> getData() {
