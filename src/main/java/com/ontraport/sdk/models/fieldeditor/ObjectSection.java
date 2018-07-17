f077fc7948d37606778843f395ae28a28add0804
Evan Jarrett <ejfirestar00@gmail.com>
2018-07-16 21:34:44 -0700

    add getter for name and description for ObjectSection

--- src/main/java/com/ontraport/sdk/models/fieldeditor/ObjectSection.java
+++ src/main/java/com/ontraport/sdk/models/fieldeditor/ObjectSection.java
@@ -32,6 +32,14 @@ public class ObjectSection implements Requestable {
         }
     }
 
+    public String getName() {
+        return _name;
+    }
+
+    public String getDescription() {
+        return _description;
+    }
+
     public void setDescription(String description) {
         _description = description;
     }
